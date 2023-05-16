f7bbb7209ed9364426b639ca556d1563e7952cdd
aniluniquex <103011235+aniluniquex@users.noreply.github.com>
2023-05-16 21:43:38 +0530

    Add files via upload
    about input and output

---
+++ DAY 4/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1,num2,product;
+
+    cout<<"Enter the num1:"<<endl;
+    cin>>num1;
+    cout<<"Enter the num2:"<<endl;
+    cin>>num2;
+
+
+    product = num1 * num2;
+
+    cout<<"The product of two num is "<<product<<endl;
+
+
+    return 0;
+    
+}
+
+
+// cin is used for input and cout for output
+// >> is exertion operator and << is insertion operator
