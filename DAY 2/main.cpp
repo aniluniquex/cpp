2a4f0ee3a607d871eedc67ca32e2d2e244e1b9a1
aniluniquex <103011235+aniluniquex@users.noreply.github.com>
2023-05-16 12:02:10 +0530

    About file
    This file explains about variables and data types and various comment lines

---
+++ DAY 2/main.cpp
@@ -0,0 +1,25 @@
+// variables and datatypes
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    int b = 6;
+
+    int sum = a+b;
+
+    cout<<sum;
+
+
+    return 0;
+}
+
+
+// This is a single line comment
+
+/*This is
+    multi line
+        commentt*/
+        
