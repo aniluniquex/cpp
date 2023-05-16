f02b2a9471a71e6232981a0e9de0faf74a464309
aniluniquex <103011235+aniluniquex@users.noreply.github.com>
2023-05-16 11:39:44 +0530

    Add files via upload
    This file describes about the introduction of cpp and basic structure of cpp

---
+++ DAY 1/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    cout<<"Hello world";
+
+
+    return 0;
+
+}
+
+/*
+include add the libraries and iostream is the library
+
+int is return type and main the function and main cannot be used for other uses becauses this is reserved keyword
+
+cout gives the output
+cin takes input
+
+return 0 is an indication that our program is successfully completed and there is no more program and compiler terminates the program at return 0
+
+{ --> This is opening of a function
+} --> This is closing of a functiom
+
+; is used after writing a line which means the line is ended
+
+
+*/
